d0f575a10311e680db927fe8abc01a07b906d89d
wjbarberjr <wiba6472@colorado.edu>
2023-11-05 02:30:24 +0000

    Trouble shoot insert route

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def inserting():
         ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
         ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
         ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
-        ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+        ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2)
         );
         ''')
     conn.commit()
