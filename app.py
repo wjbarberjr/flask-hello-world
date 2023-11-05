cd2e8e4850e0414cb79640c9145eb18049dcf4cb
wjbarberjr <wiba6472@colorado.edu>
2023-11-05 01:47:36 +0000

    Updated test_db route

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, psycopg2
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgres://wiba6472_lab10_db_user:xKD5qwOHsvmx26un0PZPkWoIdAF0IE9a@dpg-cl3evjiuuipc738c6tkg-a/wiba6472_lab10_db")
+    conn.close()
+    return 'Database Connection Successful'
